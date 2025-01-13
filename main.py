72ce133dd502440ffc4dbb1f5c27d779781daebf
fanshinglearn <fanshinglearn@gmail.com>
2025-01-13 18:02:46 +0800

    main

---
+++ main.py
@@ -0,0 +1,6 @@
+from playlist_to_excel import playlist_to_excel
+
+url = 'https://www.youtube.com/playlist?list=PLSCgthA1Anif1w6mKM3O6xlBGGypXtrtN'
+# url = input('請輸入 Youtube 撥放清單連結： ')
+
+playlist_to_excel(url)
